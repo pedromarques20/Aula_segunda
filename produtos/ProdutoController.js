26a3e0ed02b9948e7fb2091715d54618fb676b81
Pedro Marques <pedrohsmarques18@gmail.com>
2023-10-11 11:54:39 -0300

    Aula 11-10

--- produtos/ProdutoController.js
+++ produtos/ProdutoController.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Produto = require("./Produto");
+const bodyParser = require("body-parser");
 
+router.use(bodyParser.urlencoded({extended : false}));
+router.use(bodyParser.json());
 
-router.get("/produto", (req, res)=>{
-    res.render("produto");
+router.get("/produto/novo", (req, res)=>{
+    res.render("produtos/novo");
 });
 
 router.post("/salvarProduto", (req, res)=>{
@@ -18,11 +21,56 @@ router.post("/salvarProduto", (req, res)=>{
     });
 });
 
-router.get("/produtoslist", (req, res)=>{
+router.get("/produtos", (req, res)=>{
     Produto.findAll({raw: true}).then(produtos=>{
-        console.log(produtos);
+        res.render("produtos/index", {
+            produtos: produtos
+        });
     });
-    res.render("index");
+    
 });
 
-module.exports = router
+router.post("/produto/delete", (req, res)=>{
+    var id = req.body.id;
+    if(id != undefined){
+       if (!isNaN(id)){
+            Produto.destroy({
+                where : {
+                    id: id
+                }
+            }).then(()=> {
+                res.redirect("/produtos");
+            })
+       }else{
+            res.redirect("/");
+       }
+    }else{
+        res.redirect("/");
+    }
+});
+
+router.get("/produto/edit/:id", (req, res)=> {
+    var id = req.params.id;
+    Produto.findByPk(id).then(produto =>{
+        res.render("/produtos/edit" < {
+            produto : produto
+        });
+    })
+});
+
+router.post("/produto/atualizar", (req, res)=>{
+    var id = req.body.id;
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+
+    Produto.update(
+        {titulo : titulo, descricao : descricao},{
+        where: {
+            id : id
+        }
+    }).then(() => {
+        res.redirect("/produtos");
+    });
+});
+
+module.exports = router;
